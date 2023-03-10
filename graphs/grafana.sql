1339b84035314d347425ad8df776f0fe0338512d
Appuchia <appuchia@appu.ltd>
2023-03-10 12:18:29 +0100

    ftr: Update query SQL

--- graphs/grafana.sql
+++ graphs/grafana.sql
@@ -1,4 +1,4 @@
 WITH converted AS (
-   SELECT precio_gasoleo_a, precio_gasolina_95,  date || 'T00:00:00Z' AS datetime FROM Prices
+   SELECT precio_gasoleo_a, precio_gasolina_95,  date || 'T00:00:00Z' AS time FROM Prices
 )
-SELECT datetime, avg(precio_gasoleo_a), avg(precio_gasolina_95) FROM converted GROUP BY datetime
+SELECT time, avg(precio_gasoleo_a), avg(precio_gasolina_95) FROM converted GROUP BY time
