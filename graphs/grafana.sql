4b5701d9e28d136097a54da22c54dc46e1bf197a
Appuchia <appuchia@appu.ltd>
2023-03-16 13:20:50 +0100

    fix: Remove double spacing

--- graphs/grafana.sql
+++ graphs/grafana.sql
@@ -1,4 +1,4 @@
 WITH converted AS (
-   SELECT precio_gasoleo_a, precio_gasolina_95,  date || 'T00:00:00Z' AS time FROM Prices
+   SELECT precio_gasoleo_a, precio_gasolina_95, date || 'T00:00:00Z' AS time FROM Prices
 )
 SELECT time, avg(precio_gasoleo_a), avg(precio_gasolina_95) FROM converted GROUP BY time
